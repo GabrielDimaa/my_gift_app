ee88d065c57ddcfd60e4aa4550c4c149a7f8157c
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-04-29 23:07:54 -0300

    Tests em External para delete wishes

--- test/layers/external/datasources/firebase_wish_datasource_test.dart
+++ test/layers/external/datasources/firebase_wish_datasource_test.dart
@@ -4,6 +4,7 @@ import 'package:desejando_app/layers/external/helpers/errors/external_error.dart
 import 'package:desejando_app/layers/infra/models/wish_model.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../infra/models/model_extension.dart';
 import '../../infra/models/model_factory.dart';
@@ -359,6 +360,120 @@ void main() {
 
       mockFirebaseException("UNKNOWN");
       future = sut.update(wish);
+      expect(future, throwsA(isA<UnexpectedExternalError>()));
+    });
+  });
+
+  group("delete", () {
+    late FirebaseWishDataSource sut;
+    late FirestoreFirestoreSpy firestore;
+
+    final String wishId = faker.guid.guid();
+
+    void mockFirebaseException(String code) => firestore.mockDeleteError(FirebaseException(plugin: "any_plugin", code: code));
+
+    setUp(() {
+      firestore = FirestoreFirestoreSpy.delete();
+      sut = FirebaseWishDataSource(firestore: firestore);
+    });
+
+    test("Deve chamar Delete com sucesso", () async {
+      await sut.delete(wishId);
+      verify(() => firestore.collectionReferenceStubby.doc(wishId).delete());
+    });
+
+    test("Deve throw AbortedExternalError se delete() retornar FirebaseException com code ABORTED e FAILED_PRECONDITION", () {
+      mockFirebaseException("ABORTED");
+      Future future = sut.delete(wishId);
+      expect(future, throwsA(isA<AbortedExternalError>()));
+
+      mockFirebaseException("FAILED_PRECONDITION");
+      future = sut.delete(wishId);
+      expect(future, throwsA(isA<AbortedExternalError>()));
+    });
+
+    test("Deve throw AlreadyExistsExternalError se delete() retornar FirebaseException com code ALREADY_EXISTS", () {
+      mockFirebaseException("ALREADY_EXISTS");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<AlreadyExistsExternalError>()));
+    });
+
+    test("Deve throw CancelledExternalError se delete() retornar FirebaseException com code CANCELLED", () {
+      mockFirebaseException("CANCELLED");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<CancelledExternalError>()));
+    });
+
+    test("Deve throw InternalExternalError se delete() retornar FirebaseException com code INTERNAL", () {
+      mockFirebaseException("INTERNAL");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<InternalExternalError>()));
+    });
+
+    test("Deve throw InvalidArgumentExternalError se delete() retornar FirebaseException com code INVALID_ARGUMENT", () {
+      mockFirebaseException("INVALID_ARGUMENT");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<InvalidArgumentExternalError>()));
+    });
+
+    test("Deve throw NotFoundExternalError se delete() retornar FirebaseException com code NOT_FOUND", () {
+      mockFirebaseException("NOT_FOUND");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<NotFoundExternalError>()));
+    });
+
+    test("Deve throw PermissionDeniedExternalError se delete() retornar FirebaseException com code PERMISSION_DENIED", () {
+      mockFirebaseException("PERMISSION_DENIED");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<PermissionDeniedExternalError>()));
+    });
+
+    test("Deve throw ResourceExhaustedExternalError se delete() retornar FirebaseException com code RESOURCE_EXHAUSTED", () {
+      mockFirebaseException("RESOURCE_EXHAUSTED");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<ResourceExhaustedExternalError>()));
+    });
+
+    test("Deve throw UnauthenticatedExternalError se delete() retornar FirebaseException com code UNAUTHENTICATED", () {
+      mockFirebaseException("UNAUTHENTICATED");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<UnauthenticatedExternalError>()));
+    });
+
+    test("Deve throw UnavailableExternalError se delete() retornar FirebaseException com code UNAVAILABLE", () {
+      mockFirebaseException("UNAVAILABLE");
+      final Future future = sut.delete(wishId);
+
+      expect(future, throwsA(isA<UnavailableExternalError>()));
+    });
+
+    test("Deve throw UnexpectedExternalError se delete() retornar FirebaseException com code DATA_LOSS, DEADLINE_EXCEEDED, OUT_OF_RANGE, UNIMPLEMENTED e UNKNOWN", () {
+      mockFirebaseException("DATA_LOSS");
+      Future future = sut.delete(wishId);
+      expect(future, throwsA(isA<UnexpectedExternalError>()));
+
+      mockFirebaseException("DEADLINE_EXCEEDED");
+      future = sut.delete(wishId);
+      expect(future, throwsA(isA<UnexpectedExternalError>()));
+
+      mockFirebaseException("OUT_OF_RANGE");
+      future = sut.delete(wishId);
+      expect(future, throwsA(isA<UnexpectedExternalError>()));
+
+      mockFirebaseException("UNIMPLEMENTED");
+      future = sut.delete(wishId);
+      expect(future, throwsA(isA<UnexpectedExternalError>()));
+
+      mockFirebaseException("UNKNOWN");
+      future = sut.delete(wishId);
       expect(future, throwsA(isA<UnexpectedExternalError>()));
     });
   });
