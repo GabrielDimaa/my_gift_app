94dd7f74abf466a7a8c20769be95d2786d2597eb
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-07-04 00:01:46 -0300

    Adicionado firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app_theme.dart';
 import 'di/injection.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await Injection().setup();
 
